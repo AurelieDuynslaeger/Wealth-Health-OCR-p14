30b1d77bc1d9c7872a9c868df3e6390c03d4e912
AureLeeLi <aduynslaeger@gmail.com>
2024-09-23 15:52:38 +0200

    ✅ react-datepicker

--- src/components/EmployeeForm.jsx
+++ src/components/EmployeeForm.jsx
@@ -1,17 +1,21 @@
 import { useForm } from 'react-hook-form';
-// import 'react-calendar/dist/Calendar.css';
 import { useDispatch } from 'react-redux';
 import { addEmployee } from '../redux/slices/employeeSlice';
 import { useState } from 'react';
 import Select from 'react-select';
 import { optionsStates, optionsDepartement } from '../lib/optionsValues';
 import { Button } from '@mui/material';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const EmployeeForm = () => {
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, setValue } = useForm();
     const dispatch = useDispatch();
     const [selectedOptionDep, setSelectedOptionDep] = useState(null);
     const [selectedOptionStat, setSelectedOptionStat] = useState(null);
+    const [birthDate, setBirthDate] = useState(null);
+    const [startDate, setStartDate] = useState(null);
+
 
     const onSubmit = (data) => {
       dispatch(addEmployee({ 
@@ -27,8 +31,28 @@ const EmployeeForm = () => {
     <form onSubmit={handleSubmit(onSubmit)}>
         <input {...register('firstName')} placeholder="First Name" required />
         <input {...register('lastName')} placeholder="Last Name" required />
-        <input {...register('dateOfBirth')} type="date" required />
-        <input {...register('startDate')} type="date" required />
+        <DatePicker
+                selected={birthDate}
+                onChange={(date) => {
+                    setBirthDate(date);
+                    setValue('dateOfBirth', date);
+                }}
+                placeholderText="Date of Birth"
+                dateFormat="yyyy/MM/dd"
+                isClearable
+            />
+        {/* <input {...register('dateOfBirth')} type="date" required /> */}
+        <DatePicker
+                selected={startDate}
+                onChange={(date) => {
+                    setStartDate(date);
+                    setValue('startDate', date); // Mettre à jour le champ du formulaire
+                }}
+                placeholderText="Start Date"
+                dateFormat="yyyy/MM/dd"
+                isClearable
+            />
+        {/* <input {...register('startDate')} type="date" required /> */}
         <input {...register('street')} placeholder="Street" required />
         <input {...register('city')} placeholder="City" required />
         <Select
